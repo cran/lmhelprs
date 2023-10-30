a70c4f9de522de0a8da27559cd07e066738094f8
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-10-30 13:10:02 +0000

    version 0.2.0

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+# lmhelprs 0.2.0
+
+- Added `data_test1`, a test dataset. (0.0.0.9002)
+- Added `hierarchical()`. (0.0.0.9003)
+- Added `hierarchical_lm()` and the
+  `print` method for its output. (0.0.0.9004, 0.0.0.9005, 0.0.0.9006)
+- Added `test_highest()` and
+  `highest_order()`. (0.0.0.9007)
+- Added a vignetted. (0.0.0.9008)
+- First submission to CRAN. (0.1.0, 0.2.0)
---
+++ inst/doc/lmhelprs.R
@@ -0,0 +1,40 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## -----------------------------------------------------------------------------
+library(lmhelprs)
+data(data_test1)
+lm1a <- lm(y ~ x1 + x2, data_test1)
+lm1b <- lm(y ~ x1 + x2 + x3 + x4, data_test1)
+lm1c <- lm(y ~ x1 + x2 + x3 + x4 + cat2, data_test1)
+
+## -----------------------------------------------------------------------------
+hierarchical_lm(lm1b, lm1a, lm1c)
+
+## ----error = TRUE-------------------------------------------------------------
+lm2a <- lm(y ~ x1 + x2, data_test1)
+lm2b <- lm(y ~ x1 + x3 + x4, data_test1)
+hierarchical_lm(lm2a, lm2b)
+
+## -----------------------------------------------------------------------------
+lm_mod <- lm(y ~ x1 + cat2 + cat1 + cat2:cat1, data_test1)
+summary(lm_mod)
+
+## -----------------------------------------------------------------------------
+test_highest(lm_mod)
+
+## ----echo = FALSE-------------------------------------------------------------
+mod_test <- test_highest(lm_mod)
+
+## -----------------------------------------------------------------------------
+lm_mod3 <- lm(y ~ x1 + x2 + x3*x4*cat2, data_test1)
+summary(lm_mod3)
+test_highest(lm_mod3)
+
+## ----error = TRUE-------------------------------------------------------------
+lm_mod2 <- lm(y ~ x1 + x2*x3 + x2*x4, data_test1)
+test_highest(lm_mod2)
+
